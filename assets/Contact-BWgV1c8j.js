d7c857526234f6a05db06725e3b4c550cd50d49b
Stickywest <stephengioche30@gmail.com>
2025-05-24 12:46:38 +0300

    Updates

---
+++ assets/Contact-BWgV1c8j.js
@@ -0,0 +1,2 @@
+import{j as o}from"./vendor-CkNeAvwQ.js";const s=()=>o.jsxs("main",{children:[o.jsx("h1",{children:"Contact Us"}),o.jsx("p",{children:"Get in touch with us for more information on our services and solutions."})]});export{s as default};
+//# sourceMappingURL=Contact-BWgV1c8j.js.map
